abd3b6bb7b9aedb7d7fb5cdb8a6892db06cd0e5e
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2022-02-17 17:17:51 +0700

    test: MemoryReadStream diff chunk sizes

--- src/MemoryReadStream.test.ts
+++ src/MemoryReadStream.test.ts
@@ -1,7 +1,7 @@
 import MemoryReadStream from './MemoryReadStream'
 
 describe('MemoryReadStream', () => {
-  it('is a readable stream that stores the data in memory', async () => {
+  it.each([null, 2, 8, 16, 32])('is a readable stream that stores the data in memory (%d chunk size)', async (chunkSize) => {
     const content = 'This is a content.'
 
     const stream = new MemoryReadStream(Buffer.from(content, 'utf8'))
@@ -12,8 +12,14 @@ describe('MemoryReadStream', () => {
       stream.on('end', () => resolve(content))
       stream.on('readable', () => {
         let chunk: Uint8Array
-        while (null !== (chunk = stream.read())) {
-          content += chunk.toString()
+        if (chunkSize === null) {
+          while (null !== (chunk = stream.read())) {
+            content += chunk.toString()
+          }
+        } else {
+          while (null !== (chunk = stream.read(chunkSize))) {
+            content += chunk.toString()
+          }
         }
       })
     })
